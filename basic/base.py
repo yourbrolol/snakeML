cbd7e06b24dc2d7d3b258e889a68f3ca1f506369
yourbrolol <andriy.bondar.2203@gmail.com>
2026-07-06 14:11:40 +0300

    feat(base): add Neuron class and NeuronBasedLinear class for experimental testing.

--- basic/base.py
+++ basic/base.py
@@ -1,5 +1,45 @@
 from .structs import Vector
 
+class Neuron():
+    def __init__(self, input_dim):
+        self.w = Vector()
+        self.bias = Vector()
+        self.input = None
+        self.d_w = None
+        self.d_b = None
+    def forward(self, input_data): raise NotImplementedError
+    def backward(self, output_grad): raise NotImplementedError
+
+class LinearNeuron(Neuron):
+    def __init__(self, input_dim):
+        self.w = Vector([0.1] * input_dim)
+        self.bias = Vector(0.1)
+        self.input = None
+        self.d_w = None
+        self.d_b = None
+    def forward(self, input_data):
+        self,input = input_data
+        return input_data * self.w + self.b
+    def backward(self, output_grad):
+        self.d_w = output_grad / self.input
+        self.d_b = output_grad
+        return output_grad * self.w
+
+class NeuronBasedLinear:
+    def __init__(self, input_dim, output_dim):
+        self.neurons = [Neuron(input_dim) for _ in range(output_dim)]
+
+    def forward(self, input_data):
+        return Vector([neuron.forward(input_data) for neuron in self.neurons])
+
+    def backward(self, output_gradient):
+        input_gradient = Vector([0] * len(self.neurons[0].w))
+        
+        for i, neuron in enumerate(self.neurons):
+            input_gradient += neuron.backward(output_gradient[i])
+            
+        return input_gradient
+
 class Layer():
     def __init__(self):
         self.params = {}
