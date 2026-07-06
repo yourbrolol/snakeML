b849a3935120ea052f3427cf23003ea22d5eade8
yourbrolol <andriy.bondar.2203@gmail.com>
2026-07-06 14:33:12 +0300

    feat(loss): add Loss and MSE classes, to be updated.

---
+++ basic/loss.py
@@ -0,0 +1,11 @@
+class Loss:
+    def __init__(self, name):
+        self.name = name
+    def forward(self, y_pred, y_true): raise NotImplementedError
+    def backward(self, y_pred, y_true): raise NotImplementedError
+
+class MSE(Loss):
+    def __init__(self):
+        super().__init__("MSELoss")
+    def forward(self, y_pred, y_true): return (1/2) * (y_pred-y_true)**2
+    def backward(self, y_pred, y_true): return (y_pred - y_true)
