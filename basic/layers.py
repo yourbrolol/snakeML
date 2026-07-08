b508930d608bbe3cd54212f722b77737ed91ad04
yourbrolol <andriy.bondar.2203@gmail.com>
2026-07-08 14:24:59 +0300

    fix(layers): fix matrix-vector mul issues Equalize shapes needed for matrix-vector mul.

--- basic/layers.py
+++ basic/layers.py
@@ -11,13 +11,13 @@ class Layer():
 class Linear(Layer):
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.params['w'] = Array([0.1] * input_dim)
-        self.params['b'] = Array([0.1])
+        self.params['w'] = Array([[0.1] * input_dim for _ in range(output_dim)])
+        self.params['b'] = Array([0.1] * output_dim)
     def forward(self, input_data):
         self.input = Array(input_data) if not isinstance(input_data, Array) else input_data
-        print("cur", self.input, self.params['w'], self.params['b'])
-        print("res", ((self.input @ self.params['w']) + self.params['b']))
-        return (self.input @ self.params['w']) + self.params['b']
+        print("cur", self.input, self.params['w'], self.params['b'], self.params['w'].ndim, self.input.ndim)
+        print("res", ((self.params['w'].dot(self.input)) + self.params['b']))
+        return (self.params['w'].dot(self.input)) + self.params['b']
     def backward(self, output_grad):
         self.params['w'] -= output_grad * self.input
         self.params['b'] -= output_grad
