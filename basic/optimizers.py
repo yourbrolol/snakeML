bfa0227979409e2b29dab75acfe9097d31023a74
yourbrolol <andriy.bondar.2203@gmail.com>
2026-07-24 18:02:19 +0300

    feat(optimizers): implement Adam optimizer.

--- basic/optimizers.py
+++ basic/optimizers.py
@@ -30,3 +30,42 @@ class SGD(Optimizer):
             if not layer.grads:
                 continue
             layer.grads = {}
+
+class Adam(Optimizer):
+    def __init__(self, layers=None, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        """Adam optimizer."""
+        super().__init__(layers, lr)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = {}
+        self.v = {}
+        self.t = 0
+
+    def step(self):
+        """Perform a single optimization step updating layer parameters."""
+        logger.debug("optimizer step", layer_count=len(self.layers), lr=self.lr)
+        self.t += 1
+        for layer in self.layers:
+            if not layer.params or not layer.grads:
+                logger.debug("optimizer skipping layer with no params or grads", layer=layer)
+                continue
+            for key in layer.params:
+                if key in layer.grads:
+                    if key not in self.m:
+                        self.m[key] = 0
+                        self.v[key] = 0
+                    grad = layer.grads[key]
+                    self.m[key] = self.beta1 * self.m[key] + (1 - self.beta1) * grad
+                    self.v[key] = self.beta2 * self.v[key] + (1 - self.beta2) * (grad ** 2)
+                    m_hat = self.m[key] / (1 - self.beta1 ** self.t)
+                    v_hat = self.v[key] / (1 - self.beta2 ** self.t)
+                    layer.params[key] -= self.lr * m_hat / (v_hat ** 0.5 + self.epsilon)
+
+    def zero_grad(self):
+        """Clear gradients of all managed layers."""
+        logger.debug("optimizer zero_grad", layer_count=len(self.layers))
+        for layer in self.layers:
+            if not layer.grads:
+                continue
+            layer.grads = {}
